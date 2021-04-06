1c32694d8036cd926e9b560d5eda6b542b049a5d
ireneuszd <34318556+ireneuszd@users.noreply.github.com>
2021-04-07 00:00:14 +0200

    Update main.py
    Implement main logic

--- ReceiverService/main.py
+++ ReceiverService/main.py
@@ -1,1 +1,13 @@
+from flask import Flask
+from flask_restplus import Resource, Api
 
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/api/v1/info')
+class Info(Resource):
+    def get(self):
+        return {"Receiver": "Cisco is the best!"}
+
+if __name__ == '__main__':
+    app.run(debug=False)
