fbb3e5179674574a0375ebfa1473d5c153355df8
ireneuszd <34318556+ireneuszd@users.noreply.github.com>
2021-04-06 23:47:43 +0200

    Update main.py
    Write down main logic

--- PingService/main.py
+++ PingService/main.py
@@ -1,1 +1,20 @@
+from flask import Flask, request
+from flask_restplus import Resource, Api
+import requests
 
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/api/v1/ping')
+class Ping(Resource):
+    def post(self):
+        url = request.form['url']
+        return requests.get(url, verify=False).text
+
+@api.route('/health')
+class Health(Resource):
+    def get(self):
+        return True
+
+if __name__ == '__main__':
+    app.run(debug=False)
