d90fd5ca98d6c92be71237eabcae948b131ed652
ireneuszd <34318556+ireneuszd@users.noreply.github.com>
2021-04-19 16:37:10 +0200

    Avoid empty response from server

--- ReceiverService/main.py
+++ ReceiverService/main.py
@@ -10,4 +10,4 @@ class Info(Resource):
         return {"Receiver": "Cisco is the best!"}
 
 if __name__ == '__main__':
-    app.run(debug=False, port=8080)
+    app.run(host='0.0.0.0', debug=False, port=8080)
